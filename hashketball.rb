b63196fc744f4e3d5586d43cfb34fed0ec50e5b3
Farhanul Karim <farhanulkarim3770@gmail.com>
2020-08-10 18:29:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,62 @@
+# Wrirequire 'pry'
 # Write your code here!
+def game_hash
+	games = {
+		:home => {
+			:team_name => "Brooklyn Nets",
+			:colors => ["Black","White"],
+			:players => [
+								{:player_name => "Alan Anderson",
+								:number => 0,
+								:shoe => 16,
+								:num_points_scored => 22, 
+								:rebounds => 12, 
+								:assists => 12, 
+								:steals => 3, 
+								:blocks => 1, 
+								:slam_dunks => 1 },
+								
+								{:player_name => "Reggie Evans",
+								:number => 30,
+								:shoe => 14,
+								:num_points_scored => 12, 
+								:rebounds => 12, 
+								:assists => 12, 
+								:steals => 12, 
+								:blocks => 12, 
+								:slam_dunks => 7},
+								
+								{:player_name => "Brook Lopez",
+								:number => 11,
+								:shoe => 17,
+								:num_points_scored => 17, 
+								:rebounds => 19, 
+								:assists => 10, 
+								:steals => 3, 
+								:blocks => 1, 
+								:slam_dunks => 15},
+								
+								{:player_name => "Mason Plumlee",
+								:number => 1,
+								:shoe => 19,
+								:num_points_scored => 26, 
+								:rebounds => 12, 
+								:assists => 6, 
+								:steals => 3, 
+								:blocks => 8, 
+								:slam_dunks => 5},
+								
+								{:player_name => "Jason Terry",
+								:number => 31,
+								:shoe => 15,
+								:num_points_scored => 19, 
+								:rebounds => 2, 
+								:assists => 2, 
+								:steals => 4, 
+								:blocks => 11, 
+								:slam_dunks => 1}]
+		},
+		:away => {
+			:team_name => "Charlotte Hornets",
+			:colors => ["Turquoise","Purple"],
+			:players => [te your code here!
